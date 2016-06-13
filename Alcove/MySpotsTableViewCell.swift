9ed1cf135e818f948f319ba5a3258d81c4ed3613
Oliver Short <oshort55@gmail.com>
2016-06-13 11:16:41 -0400

    Firebase Configured - Initial UI set up

---
+++ Alcove/MySpotsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MySpotsTableViewCell.swift
+//  Alcove
+//
+//  Created by Oliver Short on 6/13/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+import UIKit
+
+class MySpotsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Alcove/MySpotsViewController.swift
+++ Alcove/MySpotsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class MySpotsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
